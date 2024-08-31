7865fa8b7f49ed1f09becef5cb822ff3ec76c9d2
dolev6780 <dolev6780@gmail.com>
2024-08-31 10:14:24 +0300

    build update 2

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import Sec1 from "./sections/Sec1";
 import Sec4 from "./sections/Sec4";
 import Sec3Wrapper from "./sections/Sec3Wrapper";
-import Sec2Alt from "./sections/Sec2Alt";
 export default function Home() {
   return (
     <div id="sec1" className="w-full">
       <Sec1 />
-      {/* <Sec2 /> */}
-      {/* <Sec3 /> */}
-      {/* <Sec3Alt/> */}
-      <Sec2Alt/>
       <Sec3Wrapper/>
       <Sec4 />
       
